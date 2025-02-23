b395591976dd82c00ad8b09e05081eb8c6a5aab6
Sagar Patil <sagarreddypatil@gmail.com>
2025-02-23 08:05:14 -0500

    stuff worko

--- backend/main.py
+++ backend/main.py
@@ -103,22 +103,20 @@ def grift_check_endpoint(session_data: SessionData = Depends(get_session)):
     # resume_json_path = os.path.join(output_dir, "resumes_analysis.json")
 
     # save both to temp files
-    resume_pdf_tmp = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
-    resume_pdf_tmp.write(resume_data)
-    resume_pdf_tmp.flush()
-    resume_pdf_path = resume_pdf_tmp.name
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as resume_pdf_tmp:
+        resume_pdf_tmp.write(resume_data)
+        resume_pdf_tmp.flush()
+        resume_pdf_path = resume_pdf_tmp.name
 
-    resume_json_tmp = tempfile.NamedTemporaryFile(delete=False, suffix=".json")
-    resume_json_tmp.write(json.dumps(resume_json).encode())
-    resume_json_tmp.flush()
-    resume_json_path = resume_json_tmp.name
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as resume_json_tmp:
+            resume_json_tmp.write(json.dumps(resume_json).encode())
+            resume_json_tmp.flush()
+            resume_json_path = resume_json_tmp.name
 
-    # Call grift check with the correct paths
-    out_path = grift_check(resume_pdf_path, resume_json_path)
-    print(f"Generated annotated PDF at: {out_path}")
-
-    resume_pdf_tmp.close()
-    resume_json_tmp.close()
+            # Call grift check with the correct paths
+            print("calling grift check")
+            out_path = grift_check(resume_pdf_path, resume_json_path)
+            print(f"Generated annotated PDF at: {out_path}")
 
     # load out_path into a byte array
     with open(out_path, "rb") as f:
@@ -131,13 +129,13 @@ def grift_check_endpoint(session_data: SessionData = Depends(get_session)):
 
 
 @app.get("/api/pdf")
-async def get_pdf(session_data: SessionData = Depends(get_session)):
+def get_pdf(session_data: SessionData = Depends(get_session)):
     pdf_data = session_data.resume_content
     return Response(content=pdf_data, media_type="application/pdf")
 
 
 @app.get("/api/annotated-pdf")
-async def get_annot_pdf(session_data: SessionData = Depends(get_session)):
+def get_annot_pdf(session_data: SessionData = Depends(get_session)):
     pdf_data = session_data.annot_pdf
     if pdf_data is None:
         raise HTTPException(status_code=404, detail="Annotated PDF not found")
